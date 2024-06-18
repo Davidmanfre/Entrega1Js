7f653291b24780af37b52a5b016191a7e89a02ae
davidman <davidmanfredi74@gmail.com>
2024-06-18 20:18:24 -0300

    correccion segunda entrega

--- main.js
+++ main.js
@@ -7,23 +7,40 @@ const productos = [
 let carrito = [];
 let precioTotal = 0;
 
-while (true) {
-    const nombreProducto = prompt("Ingrese el nombre del producto (o escribe 'salir' para terminar):");
+  // Función para mostrar los productos
+function mostrarProductos() {
+    console.log("Productos disponibles:");
+    productos.forEach(producto => {
+    console.log(`Nombre: ${producto.nombre}, Precio: ${producto.precio}`);
+    });
+}
 
-    if (nombreProducto.toLowerCase() === "salir") {
-    break;
-    }
+  // Función para agregar un producto al carrito
+function agregarAlCarrito(nombreProducto) {
+    const producto = productos.find(producto => producto.nombre.toLowerCase() === nombreProducto.toLowerCase());
 
-    const productoEncontrado = productos.find(producto => producto.nombre === nombreProducto);
-
-    if (productoEncontrado) {
-    carrito.push(productoEncontrado);
-    precioTotal += productoEncontrado.precio;
-    console.log(`${nombreProducto} agregado al carrito`);
+    if (producto) {
+    carrito.push(producto);
+    precioTotal += producto.precio;
+    console.log(`${producto.nombre} agregado al carrito`);
     } else {
     console.log(`No se encontró el producto ${nombreProducto}`);
     }
 }
 
-console.log("Carrito:", carrito);
-console.log("Precio total:", precioTotal);
+  // Mostrar productos
+mostrarProductos();
+
+  // Bucle para agregar productos al carrito
+while (true) {
+    let nombreProducto = prompt("Ingrese el nombre del producto (o escribe 'salir' para terminar):");
+
+    if (nombreProducto.trim().toLowerCase() === "salir") {
+    break;
+    }
+
+    agregarAlCarrito(nombreProducto.trim());
+}
+// Mostrando el carrito
+console.log("Tu Carrito:", carrito);
+console.log("Precio total a pagar:", precioTotal);
